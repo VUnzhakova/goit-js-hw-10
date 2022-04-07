9935d6ebfbe9790c478a9707ac9e621d7f639a4b
Valentina Unzhakova <vunzhakova@outlook.com>
2022-04-07 21:08:07 +0300

    finaly

--- src/index.js
+++ src/index.js
@@ -5,68 +5,68 @@ import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputEl = document.getElementById('search-box');
-const listEl = document.querySelector('.country-list');
-const infoEl = document.querySelector('.country-info');
+const searchEl = document.querySelector('#search-box');
+const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
-const cleanMarkup = ref => (ref.innerHTML = '');
+searchEl.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
-const inputHandler = e => {
-  const textInput = e.target.value.trim();
+function handleInput(event) {
+  event.preventDefault();
+  const inputValue = event.target.value.trim().toLowerCase();
 
-  if (!textInput) {
-    cleanMarkup(listEl);
-    cleanMarkup(infoEl);
+  if (inputValue === '') {
+    countryInfo = '';
+    countryList = '';
     return;
   }
+  fetchCountries(`${inputValue}`)
+    .then(renderMarkup)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
 
-  fetchCountries(textInput)
-    .then(data => {
-      console.log(data);
-      if (data.length > 10) {
-        Notify.info('Too many matches found. Please enter a more specific name');
-        return;
-      }
-      renderMarkup(data);
-    })
-    .catch(err => {
-      cleanMarkup(listEl);
-      cleanMarkup(infoEl);
-      Notify.failure('Oops, there is no country with that name');
-    });
-};
-
-const renderMarkup = data => {
-  if (data.length === 1) {
-    cleanMarkup(listEl);
-    const markupInfo = createInfoMarkup(data);
-    infoEl.innerHTML = markupInfo;
+function renderMarkup(serverDataList) {
+  if (serverDataList.length > 10) {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  } else if (serverDataList.length === 1) {
+    countryInfo.innerHTML = countryInfoMarkup(serverDataList[0]);
+    countryList.innerHTML = '';
+    return;
   } else {
-    cleanMarkup(infoEl);
-    const markupList = createListMarkup(data);
-    listEl.innerHTML = markupList;
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = countryListMarkup(serverDataList);
   }
-};
+}
 
-const createListMarkup = data => {
-  return data
-    .map(
-      ({ name, flags }) =>
-        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
-    )
+function onFetchError(error) {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  Notify.failure('Oops, there is no country with that name.');
+}
+
+function countryListMarkup(serverDataList) {
+  return serverDataList
+    .map(({ flags, name }) => {
+      return `<li>
+           <img src="${flags.svg}" alt="Flag of ${name.official}" width ="70" height="50">
+           <p><b>${name.official}</b></p>
+           </li>`;
+    })
     .join('');
-};
+}
 
-const createInfoMarkup = data => {
-  return data.map(
-    ({ name, capital, population, flags, languages }) =>
-      `<h1><img src="${flags.png}" alt="${name.official}" width="40" height="40">${
-        name.official
-      }</h1>
-      <p>Capital: ${capital}</p>
-      <p>Population: ${population}</p>
-      <p>Languages: ${Object.values(languages)}</p>`,
-  );
-};
+function countryInfoMarkup({ name, flags, capital, population, languages }) {
+  const countryLanguages = Object.values(languages).join(', ');
 
-inputEl.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
+  return `<div>
+   <img src="${flags.svg}" alt="Flag of ${name.official}. width ="200" height="180">
+   <h1>${name.official}</h1>
+   </div>
+   <p><b>Capital:</b> ${capital}</p>
+   <p><b>Population:</b> ${population}</p>
+   <p><b>Languages:</b> ${countryLanguages}</p>`;
+}
